20313b46b6a39b75da8d8f32f94d7cbfa343c1d7
DELL <DELL@USMAN-BASHA>
2024-03-23 07:37:10 +0530

    first commit

---
+++ upload-download/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './App.css';
+const FileHandlingApp = () => {
+  const [file, setFile] = useState(null);
+  const [ showPreview, setShowPreview ] = useState(false);
+
+ const handleFileChange = (event) => {
+  const selectedFile = event.target.files[0];
+  setFile(selectedFile);
+  setShowPreview(false);
+ };
+const handleFileUpload = () => {
+  if (file) {
+    console.log('File upload:', file);
+    setShowPreview(true);
+  } else {
+    console.log('Please select a file first.');
+  }
+};
+const handleFileDownload = () => {
+  if (file) {
+    const blob = new Blob([file], { type: file.type });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = file.name;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    console.log('No file to download.');
+  }
+};
+return (
+  <div className="container">
+    <h1 style={{ borderBottom: '2px solid red', paddingBottom: '10px, marginBottom: 20px', color: 'indigo'}}>File Handling App</h1>
+    <input tupe="file" onChange={handleFileChange} />
+    <br />
+    <button onClick={handleFileUpload}>Upload File</button>
+    <button onClick={handleFileDownload}>Download File</button>
+    {showPreview &&(
+      <div>
+        <h3>File Preview</h3>
+        {file.type.startsWith('image/')? (
+          <img src={URL.createObjectURL(file)}
+          alt="File Preview"
+          style={{ maxWidth: '300px', maxHeight: '300px', border: '1px solid #ddd'}}
+          />
+        ):(
+          <iframe
+            title="file-preview"
+            style={{ width: '100%', height: '300px', border: '1px solid #ddd'}}
+            src= {URL.createObjectURL(file)}
+            />
+        )}
+        </div>
+    )}
+    <div className="watermark">
+      <p> sk usman</p>
+    </div>
+    </div>
+        );
+};
+export default FileHandlingApp;
